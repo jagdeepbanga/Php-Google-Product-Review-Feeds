747201660b0e1e9343154f38f172a4a45c0d0735
Jagdeep Singh <banga.jagdeep@gmail.com>
2022-10-17 21:39:29 +1100

    Added unit test

---
+++ tests/Unit/ChildElementPropertyTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace JagdeepBanga\GoogleProductReviewFeed\Tests\Unit;
+
+use JagdeepBanga\GoogleProductReviewFeed\Elements\ChildElementProperty;
+use JagdeepBanga\GoogleProductReviewFeed\Tests\TestCase;
+
+class ChildElementPropertyTest extends TestCase
+{
+    /** @test */
+    public function can_generate_xml_data_array(): void
+    {
+        $properties = new ChildElementProperty('xml_key', 'value');
+
+        $payload = $properties->getXmlStructure('');
+
+        $this->assertEquals([
+            'name' => 'xml_key',
+            'value' => 'value'
+        ], $payload);
+    }
+}
